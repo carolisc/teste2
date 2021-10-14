0dd3bc36726000404a857173517740895dcb78cb
Carol Cardoso <carolinaalcar@gmail.com>
2021-10-14 00:18:34 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Aula 4
+
+# Ex. 1
+
+nome = input('Qual o seu nome? ')
+print('Seja bem-vindo(a) ' + nome + '!')
+
+# Ex. 2
+
+dia = input('Que dia do mês você nasceu? ')
+mes = input('Em que mês você nasceu? ')
+ano = input('Em que ano você nasceu? ')
+print('Você nasceu no dia '+dia+' de '+mes+' de '+ano+'.')
+
+# Ex. 3
+
+prim = input('Primeiro número')
+seg = input('Segundo número')
+print('A soma é ', prim+seg)
